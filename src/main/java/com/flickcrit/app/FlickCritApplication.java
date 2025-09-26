be289ffb1fb0404a81052dd2558f5351b48d0b22
Artem Petrov <artemalexpetrov@gmail.com>
2025-09-26 15:14:04 +0200

    initial commit

---
+++ src/main/java/com/flickcrit/app/FlickCritApplication.java
@@ -0,0 +1,13 @@
+package com.flickcrit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickCritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlickCritApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flickcrit/app/FlickCritApplicationTests.java
@@ -0,0 +1,15 @@
+package com.flickcrit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class FlickCritApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
