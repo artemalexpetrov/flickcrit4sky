4217d53a0665b9f25b79bceba7abde34197ea06f
Artem Petrov <artemalexpetrov@gmail.com>
2025-10-01 14:38:43 +0200

    [docs] add example for page request DTO

--- src/main/java/com/flickcrit/app/infrastructure/api/model/common/PageRequestDto.java
+++ src/main/java/com/flickcrit/app/infrastructure/api/model/common/PageRequestDto.java
@@ -1,5 +1,6 @@
 package com.flickcrit.app.infrastructure.api.model.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
@@ -14,10 +15,12 @@ public final class PageRequestDto {
     private static final int DEFAULT_PAGE = 0;
     private static final int DEFAULT_PAGE_SIZE = 20;
 
+    @Schema(example = "0", description = "The page number")
     @Min(value = 0, message = "The min page value is {value}")
     @Max(value = Integer.MAX_VALUE, message = "The max page value is {value}")
     private int page = DEFAULT_PAGE;
 
+    @Schema(example = "20", description = "The number of items on the page")
     @Min(value = 1, message = "The min page size value is {value}")
     @Max(value = 50, message = "The max page size value is {value}")
     private int size = DEFAULT_PAGE_SIZE;
